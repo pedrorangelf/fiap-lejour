e351e8f59778b595a59e80dfd9e14b779db91dfd
Victor Savoi <savoi.victor@hotmail.com>
2020-06-26 18:34:31 -0300

    Proclamas

---
+++ application/src/app/pages/proclamas/proclamas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proclamas',
+  templateUrl: './proclamas.component.html',
+  styleUrls: ['./proclamas.component.scss']
+})
+export class ProclamasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
